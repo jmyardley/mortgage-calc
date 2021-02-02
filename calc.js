4ad7ea988e233ee2810f0aea3ef2a0223a63650d
James Y <35856061+jmyardley@users.noreply.github.com>
2021-02-02 16:43:12 -0500

    organize formulas

--- calc.js
+++ calc.js
@@ -2,12 +2,18 @@
 //param p = float principal
 //param r = float interest rate
 //param n = number of years 
-function calculateMortgage(p, r, n) {
-    r = convertToDecimal (r);
-    n = yearsToMonths(n);
+function calculatePayment() {
+    var cost = document.getElementById("cost").value;
+    var downPayment = document.getElementById("downPayment").value;
+    var rateInput = document.getElementById("apr").value;
+    var period = document.getElementById("period").value;
+    
+    var p = cost - downPayment;
+    var r = convertToDecimal (rateInput);
+    var n = yearsToMonths(period);
     var c = ((p * r) * Math.pow((1 + r), n)) / (Math.pow((1 + r), n) - 1);
     pmt = c.toFixed(2);
-    console.log(pmt);
+    return parseFloat(pmt);
 }
 
 function convertToDecimal(percent) {
@@ -18,5 +24,23 @@ function yearsToMonths(year) {
     return year * 12;
 }
 
-calculateMortgage (200000, 6.5, 30);
+//calculateMortgage (200000, 6.5, 30);
+
+function createSchedule() {
+    var pmt = calculatePayment();
+
+    var thisInterest = 0;
+    var thisPrincipal = 0;
+    var allInterest = 0;
+    var allPrincipal = 0;
+    var 
+} 
+
+
+var btn = document.getElementById("calculate");
+btn.onclick = function () {
+    console.log(cost, downPayment, apr, period);
+    console.log(pmt);
+}
+
  
